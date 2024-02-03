8978197f6f5d972bf078ddfdc5a6ee302b073666
DocNielsen <docnielsen@gmail.com>
2024-02-03 17:55:58 +0100

    Update install.sh
    updated install script for permissions on files and folders, simplified mkdir

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cp -r ./opt/Dell1320 /opt/Dell1320
-
-mkdir /usr/share/cups/model/Dell
-
-mkdir /usr/share/cups/model/Dell/en
-
+cp -r ./opt/Dell1320 /opt/
+mkdir -p /usr/share/cups/model/Dell/en
 cp ./Dell1320c_32bit.ppd /usr/share/cups/model/Dell/en/Dell1320c_32bit.ppd
+chown -R root:root /opt/Dell1320/ /usr/share/cups/model/Dell/en
+find /opt/Dell1320/ /usr/share/cups/model/Dell/en -type f  -exec chmod 644 {} +
+find /opt/Dell1320/ /usr/share/cups/model/Dell/en -type d  -exec chmod 755 {} +
